5d027d5a64a4a12c623eaaf50af14b0277aaf26e
duyicheng <260316514@qq.com>
2025-07-23 22:27:44 +0800

    添加價格看一下。

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -8,18 +8,23 @@ import { HeroSection } from '@/components/home/hero-section/hero-section';
 
 import { HomePageBackground } from '@/components/gradients/home-page-background';
 import { Footer } from '@/components/home/footer/footer';
+import { Pricing } from './pricing/pricing';
+import { LocalizationBanner } from './header/localization-banner';
+import { useState } from 'react';
 // import { LandingPage } from './landing/landing-page';
 
 export function HomePage() {
   const supabase = createClient();
   const { user } = useUserInfo(supabase);
-
+  const [country, setCountry] = useState('US');
   return (
     <>
+      <LocalizationBanner country={country} onCountryChange={setCountry} />
       <div>
         <HomePageBackground />
         <Header user={user} />
         <HeroSection />
+        <Pricing country={country} />
         {/* {user?.id ? <HeroSection /> : <LandingPage />} */}
         <Footer />
       </div>
